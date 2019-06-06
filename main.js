1f08d6b05cd5e33fba9e7394474d45c6db022b0c
Cheryl Hughey <cherylhughey@hotmail.com>
2019-06-05 20:08:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var mymap = L.map('mapid').setView([38.42189, -90.4647247], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 15,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiY2hlcnlsaHVnaGV5IiwiYSI6ImNqd2p3ZzM5ZTBnNmY0NXBjeXpzb3o5Y3EifQ.XG1GzUd1hUeWO6MzqHMRhw'
 }).addTo(mymap);
