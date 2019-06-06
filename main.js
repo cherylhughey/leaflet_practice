bb924436b2c53abf14cea2419daffefb54e933d7
Cheryl Hughey <cherylhughey@hotmail.com>
2019-06-05 20:58:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([38.42189, -90.4647247], 13);
+var mymap = L.map('mapid').setView([38.5505228, -90.5405763], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -7,6 +7,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiY2hlcnlsaHVnaGV5IiwiYSI6ImNqd2p3ZzM5ZTBnNmY0NXBjeXpzb3o5Y3EifQ.XG1GzUd1hUeWO6MzqHMRhw'
 }).addTo(mymap);
 
-L.marker([38.42189, -90.4647247]).addTo(map)
+L.marker([38.5505228, -90.5405763]).addTo(map)
 		.bindPopup('St. Louis County Library.<br> Fenton, MO.')
 		.openPopup();
