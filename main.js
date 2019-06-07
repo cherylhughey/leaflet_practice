5fb7f28263f47a4984c33f070b77882a0729aed3
Cheryl Hughey <cherylhughey@hotmail.com>
2019-06-06 20:09:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiY2hlcnlsaHVnaGV5IiwiYSI6ImNqd2p3ZzM5ZTBnNmY0NXBjeXpzb3o5Y3EifQ.XG1GzUd1hUeWO6MzqHMRhw'
 }).addTo(mymap);
 
-L.circleMarker(<38.5470003, -90.5470048> ,<radius: 10> );
-
-L.marker([38.5470003, -90.5470048]).addTo(mymap)
-        .bindPopup('Lone Wolf Trail.<br> Awesome view!.')
+L.marker([38.54971, -90.54021]).addTo(mymap)
+        .bindPopup('Lone Wolf Trail.<br> Trail info here.')
 		.openPopup();
 
+L.marker([38.54986, -90.53957]).addTo(mymap)
+        .bindPopup('Riverscene Trail.<br> Start here.')
+		.openPopup();
+
+/* Longitude, latitude and info found at https://dnr.mo.gov/trails/fullMap.action?xmin=-10088812.28&ymin=4653925.52&xmax=-10077050.53&ymax=4660155.14 */
