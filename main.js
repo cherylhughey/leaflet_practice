0eb7b7109f2ffae096a5cf15338a6e9d7aac2d3b
Cheryl Hughey <cherylhughey@hotmail.com>
2019-06-06 19:59:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiY2hlcnlsaHVnaGV5IiwiYSI6ImNqd2p3ZzM5ZTBnNmY0NXBjeXpzb3o5Y3EifQ.XG1GzUd1hUeWO6MzqHMRhw'
 }).addTo(mymap);
 
+L.circleMarker(<38.5470003, -90.5470048> ,<radius: 10> );
+
 L.marker([38.5470003, -90.5470048]).addTo(mymap)
         .bindPopup('Lone Wolf Trail.<br> Awesome view!.')
 		.openPopup();
