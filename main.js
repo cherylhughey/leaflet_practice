9360e66da8009844c643abc2b7053722af6d1b50
Cheryl Hughey <cherylhughey@hotmail.com>
2019-06-06 19:34:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var mymap = L.map('map').setView([38.546648, -90.539688], 13);
+var mymap = L.map('mapid').setView([38.546648, -90.539688], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -7,5 +7,5 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiY2hlcnlsaHVnaGV5IiwiYSI6ImNqd2p3ZzM5ZTBnNmY0NXBjeXpzb3o5Y3EifQ.XG1GzUd1hUeWO6MzqHMRhw'
 }).addTo(mymap);
 
-L.marker([38.5470003, -90.5470048]).addTo(map)
+L.marker([38.5470003, -90.5470048]).addTo(mymap)
 
