cfba792634f05ea6d83a5054a98d00671d16685a
Cheryl Hughey <cherylhughey@hotmail.com>
2019-06-09 14:13:29 -0500

    added circle
    from lealetjs docs

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiY2hlcnlsaHVnaGV5IiwiYSI6ImNqd2p3ZzM5ZTBnNmY0NXBjeXpzb3o5Y3EifQ.XG1GzUd1hUeWO6MzqHMRhw'
 }).addTo(mymap);
 
+var circle = L.circle([38.54971, -90.54021], {
+    color: 'green',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
 L.marker([38.54971, -90.54021]).addTo(mymap)
         .bindPopup('Lone Wolf Trail.<br> Trail info here.')
 		.openPopup();
@@ -15,4 +22,5 @@ L.marker([38.54986, -90.53957]).addTo(mymap)
         .bindPopup('Riverscene Trail.<br> Start here.')
 		.openPopup();
 
-/* Longitude, latitude and info found at https://dnr.mo.gov/trails/fullMap.action?xmin=-10088812.28&ymin=4653925.52&xmax=-10077050.53&ymax=4660155.14 */
+/* LeafletJS License on GitHub: https://github.com/Leaflet/Leaflet/blob/master/LICENSE
+Longitude, latitude and info found at https://dnr.mo.gov/trails/fullMap.action?xmin=-10088812.28&ymin=4653925.52&xmax=-10077050.53&ymax=4660155.14 */
